fc92339bff4825b0eb0f5651dca4f7f7e36d5469
stiveberol-wq <stiveberol@gmail.com>
2026-06-19 05:04:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-document.getElementById("loginForm").addEventListener("submit", function(e) {
+document.getElementById("phishingForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-
-    if(username === "admin" && password === "1234") {
-        document.getElementById("message").innerHTML = "Connexion réussie";
-    } else {
-        document.getElementById("message").innerHTML = "Identifiants incorrects";
-    }
+    document.getElementById("result").innerHTML =
+        "⚠️ This was a phishing simulation. Never enter your credentials on suspicious websites. Always verify the URL, sender, and website authenticity.";
 });
